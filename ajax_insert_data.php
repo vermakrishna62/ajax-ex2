11caa2681e3d405d6a72aec99bcff5f41441547f
vermakrishna62 <66963248+vermakrishna62@users.noreply.github.com>
2020-12-09 19:24:18 +0530

    Update ajax_insert_data.php

--- ajax_insert_data.php
+++ ajax_insert_data.php
@@ -3,7 +3,7 @@
 $name = $_POST["name"];
 $age = int($_POST["age"]);
 
-$conn = mysqli_connect('localhost','root','','login_record_system') or die("Unsuccessfull");
+$conn = mysqli_connect('remotemysql.com','nbBwIE9nVd','2M1KU0vefJ','nbBwIE9nVd') or die("Unsuccessfull");
 
 $sql = "INSERT INTO login_data(name,age,gender) VALUES ('$name','$age')";
 
